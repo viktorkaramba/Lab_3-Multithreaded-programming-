fdc24f2fb149a46e2379ff681369dedc759eaf5b
viktorkaramba <svynarvikror@gmail.com>
2021-05-06 16:37:23 +0300

    Made with the help of the pattern Strategy for creating different versions of algorithms

--- Algorithm Strassen/Algorithm_Strassen.cpp
+++ Algorithm Strassen/Algorithm_Strassen.cpp
@@ -7,7 +7,7 @@
 
 template<typename T> 
 
-void Algorithm_Strassen<T>::strassenR(Matrix<T>& A, Matrix<T>& B, Matrix<T>& C, int n) 
+void Serial_Version<T>::strassenR(Matrix<T>& A, Matrix<T>& B, Matrix<T>& C, int n)
 {
     if (n <= 1) {
         C.SetElement(A.GetData()[0][0] * B.GetData()[0][0], 0, 0);
@@ -33,20 +33,27 @@ void Algorithm_Strassen<T>::strassenR(Matrix<T>& A, Matrix<T>& B, Matrix<T>& C, 
         sum(a11, a22, aResult, newN); // a11 + a22
         sum(b11, b22, bResult, newN); // b11 + b22
         strassenR(aResult, bResult, p1, newN); // p1 = (a11+a22) * (b11+b22)
+
         sum(a21, a22, aResult, newN); // a21 + a22
         strassenR(aResult, b11, p2, newN); // p2 = (a21+a22) * (b11)
+
         subtract(b12, b22, bResult, newN); // b12 - b22
         strassenR(a11, bResult, p3, newN); // p3 = (a11) * (b12 - b22)
+
         subtract(b21, b11, bResult, newN); // b21 - b11
         strassenR(a22, bResult, p4, newN); // p4 = (a22) * (b21 - b11)
+
         sum(a11, a12, aResult, newN); // a11 + a12
         strassenR(aResult, b22, p5, newN); // p5 = (a11+a12) * (b22)
+
         subtract(a21, a11, aResult, newN); // a21 - a11
         sum(b11, b12, bResult, newN); // b11 + b12
         strassenR(aResult, bResult, p6, newN); // p6 = (a21-a11) * (b11+b12)
+
         subtract(a12, a22, aResult, newN); // a12 - a22
         sum(b21, b22, bResult, newN); // b21 + b22
-        strassenR(aResult, bResult, p7, newN); // p7 = (a12-a22) *  (b21+b22)
+        strassenR(aResult, bResult, p7, newN); // p7 = (a12-a22) * (b21+b22)
+
         sum(p3, p5, c12, newN); // c12 = p3 + p5
         sum(p2, p4, c21, newN); // c21 = p2 + p4
         sum(p1, p4, aResult, newN); // p1 + p4
@@ -67,27 +74,26 @@ void Algorithm_Strassen<T>::strassenR(Matrix<T>& A, Matrix<T>& B, Matrix<T>& C, 
 }
 
 template<typename T>
- Algorithm_Strassen<T>::Algorithm_Strassen(Matrix<T>& A, Matrix<T>& B, Matrix<T>& C, int n) 
+Serial_Version<T>::Serial_Version(Matrix<T>& A, Matrix<T>& B, Matrix<T>& C, int n)
 {
-    int m = pow(2, int(ceil(log2(n))));
-
-    Matrix<T> APrep(m, m), BPrep(m, m), CPrep(m, m);
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            APrep.SetElement(A.GetData()[i][j], i, j);
-            BPrep.SetElement(B.GetData()[i][j], i, j);
-        }
-    }
-    strassenR(APrep, BPrep, CPrep, m);
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            C.SetElement(CPrep.GetData()[i][j], i, j);
-        }
-    }
+     auto m = pow(2, int(ceil(log2(n))));
+     Matrix<T> APrep(m, m), BPrep(m, m), CPrep(m, m);
+     for (int i = 0; i < n; i++) {
+         for (int j = 0; j < n; j++) {
+             APrep.SetElement(A.GetData()[i][j], i, j);
+             BPrep.SetElement(B.GetData()[i][j], i, j);
+         }
+     }
+     strassenR(APrep, BPrep, CPrep, m);
+     for (int i = 0; i < n; i++) {
+         for (int j = 0; j < n; j++) {
+             C.SetElement(CPrep.GetData()[i][j], i, j);
+         }
+     }
 }
 
  template<typename T>
- void Algorithm_Strassen<T>::sum(Matrix<T>& A, Matrix<T>& B, Matrix<T>& C, int n) {
+ void Serial_Version<T>::sum(Matrix<T>& A, Matrix<T>& B, Matrix<T>& C, int n) {
     int i, j;
     for (i = 0; i < n; i++) {
         for (j = 0; j < n; j++) {
@@ -97,8 +103,7 @@ template<typename T>
 }
 
  template<typename T>
-
-void Algorithm_Strassen<T>::subtract(Matrix<T>& A, Matrix<T>& B, Matrix<T>& C, int n) 
+void Serial_Version<T>::subtract(Matrix<T>& A, Matrix<T>& B, Matrix<T>& C, int n)
 {
     int i, j;
     for (i = 0; i < n; i++) {
@@ -107,4 +112,5 @@ void Algorithm_Strassen<T>::subtract(Matrix<T>& A, Matrix<T>& B, Matrix<T>& C, i
         }
     }
 }
+
 #endif
--- Algorithm Strassen/Algorithm_Strassen.h
+++ Algorithm Strassen/Algorithm_Strassen.h
@@ -2,18 +2,31 @@
 #define _ALGORITHM_STRASSEN_H
 
 #include "Matrix.h"
+#include <thread>
+#include <chrono>
 
-using namespace std;
+
 
 template<typename T>
 class Algorithm_Strassen
 {
 public:
-    void strassenR(Matrix<T>& A, Matrix<T>& B, Matrix<T>& C, int n);
-    Algorithm_Strassen<T>(Matrix<T>& A, Matrix<T>& B, Matrix<T>& C, int n);
-    void sum(Matrix<T>& A, Matrix<T>& B, Matrix<T>& C, int n);
-    void subtract(Matrix<T>& A, Matrix<T>& B, Matrix<T>& C, int n);
+    virtual void strassenR(Matrix<T>& A, Matrix<T>& B, Matrix<T>& C, int n) =0;
+   // Algorithm_Strassen<T>(Matrix<T>& A, Matrix<T>& B, Matrix<T>& C, int n) = 0;
+    virtual void sum(Matrix<T>& A, Matrix<T>& B, Matrix<T>& C, int n) = 0;
+    virtual void subtract(Matrix<T>& A, Matrix<T>& B, Matrix<T>& C, int n) = 0;
 };
+
+template<typename T>
+class Serial_Version : public Algorithm_Strassen<T>
+{
+public:
+    void strassenR(Matrix<T>& A, Matrix<T>& B, Matrix<T>& C, int n) override;
+    Serial_Version<T>(Matrix<T>& A, Matrix<T>& B, Matrix<T>& C, int n);
+    void sum(Matrix<T>& A, Matrix<T>& B, Matrix<T>& C, int n) override;
+    void subtract(Matrix<T>& A, Matrix<T>& B, Matrix<T>& C, int n) override;
+};
+
 
 #include "Algorithm_Strassen.cpp"
 #endif
